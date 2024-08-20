cb630227ceabaad3db68550d24d6e4661a1604ee
ITSup96 <hunty.gabbe@unah.hn>
2024-08-19 20:48:10 -0600

    Initial commit

---
+++ src/proyectointerfaz/ProyectoInterfaz.java
@@ -0,0 +1,15 @@
+package proyectointerfaz;
+
+public class ProyectoInterfaz 
+{
+    
+
+    public static void main(String[] args) 
+    {
+        Calculadora ventana1 = new Calculadora();
+        
+        ventana1.setVisible(true);
+        
+    }
+    
+}
